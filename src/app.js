b90656aaf67996a6dac0ae88f1861b410aba69ce
Prabin Joshi <prabinjoshi68@gmail.com>
2023-07-23 10:48:48 +0545

    Started on database

--- src/app.js
+++ src/app.js
@@ -4,11 +4,62 @@ const fs = require("fs");
 
 const cors = require("cors");
 
+const mongoose = require("mongoose");
+
 const app = express();
 
 app.use(cors());
 
 const PORT = 5000;
+
+//connection string
+const mongoDbURI = "mongodb://localhost:27017/lec";
+mongoose.connect(mongoDbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  username: String,
+  fullname: String,
+  lastname: String,
+  title: String,
+  skills: [{ type: String }],
+  address: String,
+  job_type: String,
+  id: Number,
+  is_active: Boolean,
+  followers: [{ type: String }],
+  followings: [{ type: String }],
+});
+
+const User = mongoose.model("user", userSchema);
+
+User.createCollection()
+  .then((col) => {
+    console.log("Collection", col, "created");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+User.create({
+  email: "test@test.com",
+  username: "profile",
+  fullname: "Prabin Joshi",
+  lastname: "Joshi",
+  title: "Software Developer",
+  skills: ["JS", "PHP", "JAVA"],
+  address: "Kathmandu, Nepal",
+  job_type: "Full Time",
+  id: 1,
+  is_active: true,
+  followers: ["username123", "user234", "user543"],
+  followings: ["username123", "user234", "user543", "user555"],
+}).then(() => {
+  console.log("User Created");
+});
 
 //http://localhost:5000 or http://localhost:5000/
 app.get("/", (req, res) => {
