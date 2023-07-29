66e4054c6474b2c8d3b60a6c2f4b4ff3b85ad8bd
Prabin Joshi <prabinjoshi68@gmail.com>
2023-07-29 09:15:34 +0545

    Additional changes to database and use of post method

--- src/app.js
+++ src/app.js
@@ -44,23 +44,6 @@ User.createCollection()
     console.log(err);
   });
 
-User.create({
-  email: "test@test.com",
-  username: "profile",
-  fullname: "Prabin Joshi",
-  lastname: "Joshi",
-  title: "Software Developer",
-  skills: ["JS", "PHP", "JAVA"],
-  address: "Kathmandu, Nepal",
-  job_type: "Full Time",
-  id: 1,
-  is_active: true,
-  followers: ["username123", "user234", "user543"],
-  followings: ["username123", "user234", "user543", "user555"],
-}).then(() => {
-  console.log("User Created");
-});
-
 //for posts
 const userSchemaposts = new mongoose.Schema({
   title: String,
@@ -75,7 +58,7 @@ const userSchemaposts = new mongoose.Schema({
   user_id: Number,
   post_by_username: String,
   post_by_fullname: String,
-  post_date: Number,
+  post_date: String,
   comments: [{ type: String }],
 });
 
@@ -90,58 +73,58 @@ Post.createCollection()
     console.log(err);
   });
 
-Post.create(
-  {
-    title: "PHP Developer Required",
-    description: "For a client project PHP Developer is required",
-    location: "Kathmandu",
-    job_type: "Full Time",
-    pay_rate_per_hr_dollar: 10.0,
-    skills: ["PHP", "JS", "HTML"],
-    liked_by: ["test111", "test1", "test123"],
-    viewed_by: ["test111", "test1", "test123"],
-    id: 2,
-    user_id: 1,
-    post_by_username: "test123",
-    post_by_fullname: "Test User",
-    post_date: "2023-06-10T09:24:07.659034",
-    comments: [],
-  },
-  {
-    title: "Js Developer Required",
-    description: "For a client project PHP Developer is required",
-    location: "Lalitpur",
-    job_type: "Full Time",
-    pay_rate_per_hr_dollar: 10.0,
-    skills: ["PHP", "JS", "HTML"],
-    liked_by: ["test111", "test1", "test123"],
-    viewed_by: ["test111", "test1", "test123"],
-    id: 3,
-    user_id: 2,
-    post_by_username: "test321",
-    post_by_fullname: "Test User2",
-    post_date: "2023-06-10T21:51:10.643105",
-    comments: [],
-  },
-  {
-    title: "Wordpress Developer Required",
-    description: "For a client project PHP Developer is required",
-    location: "Bhaktapur",
-    job_type: "Full Time",
-    pay_rate_per_hr_dollar: 10.0,
-    skills: ["PHP", "JS", "HTML"],
-    liked_by: ["test111", "test1", "test123"],
-    viewed_by: ["test111", "test1", "test123"],
-    id: 4,
-    user_id: 3,
-    post_by_username: "test111",
-    post_by_fullname: "Test User2",
-    post_date: "2023-06-10T21:53:40.698655",
-    comments: [],
-  }
-).then(() => {
-  console.log("User created");
-});
+// Post.create([
+//   {
+//     title: "PHP Developer Required",
+//     description: "For a client project PHP Developer is required",
+//     location: "Kathmandu",
+//     job_type: "Full Time",
+//     pay_rate_per_hr_dollar: 10.0,
+//     skills: ["PHP", "JS", "HTML"],
+//     liked_by: ["test111", "test1", "test123"],
+//     viewed_by: ["test111", "test1", "test123"],
+//     id: 2,
+//     user_id: 1,
+//     post_by_username: "test123",
+//     post_by_fullname: "Test User",
+//     post_date: "2023-06-10T09:24:07.659034",
+//     comments: [],
+//   },
+//   {
+//     title: "Js Developer Required",
+//     description: "For a client project PHP Developer is required",
+//     location: "Lalitpur",
+//     job_type: "Full Time",
+//     pay_rate_per_hr_dollar: 10.0,
+//     skills: ["PHP", "JS", "HTML"],
+//     liked_by: ["test111", "test1", "test123"],
+//     viewed_by: ["test111", "test1", "test123"],
+//     id: 3,
+//     user_id: 2,
+//     post_by_username: "test321",
+//     post_by_fullname: "Test User2",
+//     post_date: "2023-06-10T21:51:10.643105",
+//     comments: [],
+//   },
+//   {
+//     title: "Wordpress Developer Required",
+//     description: "For a client project PHP Developer is required",
+//     location: "Bhaktapur",
+//     job_type: "Full Time",
+//     pay_rate_per_hr_dollar: 10.0,
+//     skills: ["PHP", "JS", "HTML"],
+//     liked_by: ["test111", "test1", "test123"],
+//     viewed_by: ["test111", "test1", "test123"],
+//     id: 4,
+//     user_id: 3,
+//     post_by_username: "test111",
+//     post_by_fullname: "Test User2",
+//     post_date: "2023-06-10T21:53:40.698655",
+//     comments: [],
+//   },
+// ]).then(() => {
+//   console.log("Posts created");
+// });
 
 //http://localhost:5000 or http://localhost:5000/
 app.get("/", (req, res) => {
@@ -154,9 +137,32 @@ app.get("/api/v1/posts", (req, res) => {
   res.status(200).send(posts);
 });
 
-app.get("/api/v1/user", (req, res) => {
-  const user = fs.readFileSync("./data/user.json", "utf-8").toString();
-  res.status(200).send(user);
+app.get("/api/v1/user", async (req, res) => {
+  // const user = fs.readFileSync("./data/user.json", "utf-8").toString();
+  const user = await User.find({ id: 1 });
+  res.status(200).send(user[0]);
+});
+
+app.post("/api/v1/user", (req, resp) => {
+  const id = req.query.id;
+  const newUser = {
+    email: "test@test.com",
+    username: "profile",
+    fullname: "Prabin Joshi",
+    lastname: "Joshi",
+    title: "Software Developer",
+    skills: ["JS", "PHP", "JAVA"],
+    address: "Kathmandu, Nepal",
+    job_type: "Full Time",
+    id: id,
+    is_active: true,
+    followers: [],
+    followings: [],
+  };
+  User.create(newUser).then((createdUser) => {
+    console.log("User Created");
+    resp.status(200).send(createdUser);
+  });
 });
 
 app.listen(PORT, () => {
